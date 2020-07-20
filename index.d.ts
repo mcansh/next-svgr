2ae5cfe5d21aa201a2cfa7158b6d0639ae753126
Logan McAnsh <logan@mcan.sh>
2020-07-20 13:22:49 -0400

    feat: add global ReactSVGElement type (#2)
    closes #1
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,11 @@
-declare module "*.svg" {
+declare module "@mcansh/next-svgr" {
   export type ReactSVGElement = React.StatelessComponent<
     React.SVGAttributes<SVGElement>
   >;
+}
 
+declare module "*.svg" {
+  import { ReactSVGElement } from "@mcansh/next-svgr";
   const content: ReactSVGElement;
   export default content;
 }
